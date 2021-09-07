3bb286b333f6ed09b37bdd8c7467fe3a23bb23a8
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-09-07 14:46:47 +0900

    [Update] Photo gallery section of the landing page. Mobile design is applied.

---
+++ client/src/components/PhotoGallery.js
@@ -0,0 +1,50 @@
+import React, {useState, useEffect} from 'react'
+import styled from 'styled-components'
+import { gallery } from '../data'
+
+const GalleryStyle = styled.div`
+  width: 100%;
+  height: ${props => `${props.height}px`};
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+
+  div {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+  img {
+    width: 100%;
+    object-fit: contain;
+  }
+`
+
+export default function PhotoGallery() {
+  const [width, setWidth] = useState(null)
+  const [height, setHeight] = useState(null)
+
+  useEffect(() => {
+    setWidth(window.innerWidth)
+    setHeight(window.innerWidth)
+  }, [])
+
+  return (
+    <GalleryStyle height={height}>
+      {/* <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>4</div> */}
+      {
+        gallery.map(photo =>
+          <div>
+            <img
+              src={width >= 500 ? photo.desktopImage : photo.mobileImage}
+              alt={photo.description}
+            />
+          </div>
+        )
+      }
+    </GalleryStyle>
+  )
+}
