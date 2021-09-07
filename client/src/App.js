9a916f151c672a21c8ab2a1e7896f908c68b7b85
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-09-07 14:47:38 +0900

    [Update] Changed the structure of the App component and made a new landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,105 +1,13 @@
-import styled, { keyframes } from 'styled-components'
-import header from '../src/assets/desktop/image-header.jpg'
-import { GlobalStyle } from '../src/globalStyle'
-import MainNav from './components/MainNav'
-import MidSet from './components/MidSet'
-import desktopEgg from './assets/desktop/image-transform.jpg'
-import desktopCup from './assets/desktop/image-stand-out.jpg'
-import MidDigitalSet from './components/MidDigitalSet'
-import desktopCherry from './assets/desktop/image-graphic-design.jpg'
-import desktopOrange from './assets/desktop/image-photography.jpg'
-import arrowDown from './assets/icon-arrow-down.svg'
-import Testimonial from './components/Testimonial'
-import Footer from './components/Footer'
-
-const MainWrapper = styled.div`
-  width: 100vw;
-  max-width: 1440px;
-`
-
-const arrowDownMove = keyframes`
-  0% {
-  }
-  50% { 
-    opacity: 1;
-    transform: translateY(10px);
-  }
-  100% {
-  }
-`
-
-const TopSection = styled.section`
-  width: 100%;
-  height: 100vh;
-  background-image: url(${header});
-  background-size: cover;
-  background-repeat: no-repeat;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  h1 {
-    position: absolute;
-    color: #fff;
-    font-size: 3em;
-    letter-spacing: 5px;
-    top: 30%;
-  }
-
-  #main-arrow-down {
-    background: transparent;
-    border: none;
-    display: inline-block;
-
-    &:hover {
-      cursor: pointer;
-      animation: ${arrowDownMove} 1s infinite;
-    }
-  }
-`
-const MidSection = styled.section`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-`
+import Header from './components/Header';
+import LandingPage from './components/LandingPage'
+import { GlobalStyles } from './GlobalStyles'
 
 function App() {
   return (
     <>
-      <GlobalStyle/>
-      <MainWrapper>
-        <MainNav />
-        <TopSection>
-          <h1>WE ARE CREATIVES</h1>
-          <button id="main-arrow-down"><img src={arrowDown} alt="scroll down arrow"/></button>
-        </TopSection>
-        <MidSection>
-          <MidSet 
-            title={"Transform your brand"}
-            text={"We are a full-service creative agency specializing in helping brands grow fast. engage your clients through compelling visuals that do most of the marketing for you."}
-            buttonText={"Learn more"}
-            image={desktopEgg}
-            imgFirst={false}
-          />
-          <MidSet 
-            title={"Stand out to the right audience"}
-            text={"Using a collaborative formula of designers, researchers, photographers, videographers, and coopywriters, we'll build and extend your brand in digital places."}
-            buttonText={"Learn more"}
-            image={desktopCup}
-            imgFirst={true}
-          />
-          <MidDigitalSet
-            title1={"Graphic Design"}
-            text1={"Great desgin makes you memorable. We deliver artwork that underscores your brand message and captures potential client's attention."}
-            image1={desktopCherry}
-            title2={"Photography"}
-            text2={"Increase your credibility by getting the most stunning, high-quality photos that improve your business image."}
-            image2={desktopOrange}
-          />
-        </MidSection>
-        <Testimonial />
-        <Footer />
-      </MainWrapper>
+      <GlobalStyles />
+      <Header />
+      <LandingPage />
     </>
   );
 }
---
+++ client/src/components/LandingPage.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import styled from 'styled-components'
+import mOrange from '../assets/mobile/image-header.jpg'
+import arrow from '../assets/icon-arrow-down.svg'
+import Mid from './Mid'
+import Testimonials from './Testimonials'
+import PhotoGallery from './PhotoGallery'
+import Footer from './Footer'
+
+const LandingPageStyle = styled.div`
+  width: 100vw;
+  height: auto;
+`
+
+const Top = styled.section`
+  width: 100%;
+  height: 65vh;
+  background-image: url(${mOrange});
+  background-size: cover;
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 2rem;
+  color: #fff;
+  h1 {
+    text-align: center;
+    font-size: 3rem;
+  }
+  button {
+    width: 100px;
+    height: 100px;
+    border: none;
+    background-color: transparent;
+    background-image: url(${arrow});
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+`
+
+export default function LandingPage() {
+  return (
+    <LandingPageStyle>
+      <Top>
+        <h1>WE ARE CREATIVES</h1>
+        <button></button>
+      </Top>
+      <Mid />
+      <Testimonials />
+      <PhotoGallery />
+      <Footer />
+    </LandingPageStyle>
+  )
+}
