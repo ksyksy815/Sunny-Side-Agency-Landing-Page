78ee70d510573788c6374040e4101e92720d48c8
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-09-07 14:38:19 +0900

    [Update] Created a data file that contains landing page contents

---
+++ client/src/data.js
@@ -0,0 +1,119 @@
+import mEgg from './assets/mobile/image-transform.jpg'
+import dEgg from './assets/desktop/image-transform.jpg'
+import mStand from './assets/mobile/image-stand-out.jpg'
+import dStand from './assets/desktop/image-stand-out.jpg'
+import mCherry from './assets/mobile/image-graphic-design.jpg'
+import dCherry from './assets/desktop/image-graphic-design.jpg'
+import mOrange from './assets/mobile/image-photography.jpg'
+import dOrange from './assets/desktop/image-photography.jpg'
+import emily from './assets/image-emily.jpg'
+import thomas from './assets/image-thomas.jpg'
+import jennie from './assets/image-jennie.jpg'
+import mMilk from './assets/mobile/image-gallery-milkbottles.jpg'
+import dMilk from './assets/desktop/image-gallery-milkbottles.jpg'
+import mSlicedOrange from './assets/mobile/image-gallery-orange.jpg'
+import dSlicedOrange from './assets/desktop/image-gallery-orange.jpg'
+import mCone from './assets/mobile/image-gallery-cone.jpg'
+import dCone from './assets/desktop/image-gallery-cone.jpg'
+import mSugar from './assets/mobile/image-gallery-sugar-cubes.jpg'
+import dSugar from './assets/desktop/image-gallery-sugar-cubes.jpg'
+import facebook from './assets/icon-facebook.svg'
+import instagram from './assets/icon-instagram.svg'
+import twitter from './assets/icon-twitter.svg'
+import pinterest from './assets/icon-pinterest.svg'
+
+export const midContents1 = [
+  {
+    title: 'Transform your brand',
+    text: 'We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.',
+    mobileImage: mEgg,
+    desktopImage: dEgg,
+  },
+  {
+    title: 'Stand out to the right audience',
+    text: `Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we'll build and extend your brand in digital places.`,
+    mobileImage: mStand,
+    desktopImage: dStand,
+  }
+]
+
+export const midContents2 = [
+  {
+    title: 'Graphic Desgin',
+    text: `Great design makes you memorable. We deliver artwork that underscores your barnd message and captures potential clients' attention.`,
+    mobileImage: mCherry,
+    desktopImage: dCherry,
+    fontColor: `hsl(167, 40%, 24%)`,
+  },
+  {
+    title: 'Photography',
+    text: 'Increase your credibility by getting the most stunning, high-quality photos that improve your business image.',
+    mobileImage: mOrange,
+    desktopImage: dOrange,
+    fontColor: `hsl(198, 62%, 26%)`,
+  },
+]
+
+export const testimonials = [
+  {
+    image: emily,
+    text: `We put our trust in Sunnyside and they delivered, making sure our needs were met and deadlines were always hit.`,
+    name: `Emily R.`,
+    position: `Marketing Director`,
+  },
+  {
+    image: thomas,
+    text: `Sunnyside's enthusiasm coupled with their keen interest in our brand's success mde it a satisfying and enjoyable experience.`,
+    name: `Thomas S.`,
+    position: `Chief Operating Officer`,
+  },
+  {
+    image: jennie,
+    text: `Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!`,
+    name: `Jennie F.`,
+    position: `Business Owner`,
+  },
+]
+
+export const gallery = [
+  {
+    mobileImage: mMilk,
+    desktopImage: dMilk,
+    description: 'milk',
+  },
+  {
+    mobileImage: mSlicedOrange,
+    desktopImage: dSlicedOrange,
+    description: 'sliced orarnge',
+  },
+  {
+    mobileImage: mCone,
+    desktopImage: dCone,
+    description: 'ice cream cone',
+  },
+  {
+    mobileImage: mSugar,
+    desktopImage: dSugar,
+    description: 'sugar cubes',
+  },
+]
+
+export const socialMediaIcons = [
+  {
+    name: 'facebook',
+    image: facebook,
+  },
+  {
+    name: 'instagram',
+    image: instagram
+  },
+  {
+    name: 'twitter',
+    image: twitter
+  },
+  {
+    name: 'pinterest',
+    image: pinterest
+  },
+
+]
