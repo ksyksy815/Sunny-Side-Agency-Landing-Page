c1266a51c7daf870a268e5630525899eeb9f60f1
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-09-08 21:45:46 +0900

    [Fix] Added button CSS

--- client/src/components/LearnMoreButton.js
+++ client/src/components/LearnMoreButton.js
@@ -3,6 +3,10 @@ import styled from 'styled-components'
 
 const LearnMoreBtn = styled.button`
   font-weight: bold;
+  background: ${props=> `linear-gradient(180deg, transparent 40% , ${props.color})`} ;
+  border: none;
+  padding: 0.5rem 1rem;
+  border-radius: 30px;
 `
 
 export default function LearnMoreButton( {color}) {
